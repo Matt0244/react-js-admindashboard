d9adf96adb588a84c4f11bb8e85e14d64f90b052
Matt0244 <87352633+Matt0244@users.noreply.github.com>
2024-08-27 23:27:46 +0930

    section 6

--- src/pages/matt/matt.jsx
+++ src/pages/matt/matt.jsx
@@ -1,39 +1,27 @@
-import { Fragment, useState } from "react";
-import { Dialog, DialogPanel } from "@headlessui/react";
-import {
-  ArrowPathIcon,
-  Bars3Icon,
-  CloudArrowUpIcon,
-  FingerPrintIcon,
-  LockClosedIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
-import { CheckIcon } from "@heroicons/react/20/solid";
+import React from "react";
 import logoWorkcation from "../../assets/images/logoWorkcation.svg";
 import beachWork from "../../assets/images/beach-work.jpg";
+import popularDestination from "../../data/popularDestinations.js";
+import DestinationCard from "../../components/DestinationCard.jsx";
+import toronto from '../../assets/images/toronto.jpg'
 
 export default function Matt() {
   return (
-    <div className="bg-gray-300 min-h-screen  ">
-      <div className="bg-gray-100 grid lg:grid-cols-2 2xl:grid-cols-5 ">
-        {/* 左面 */}
-        <div className=" px-8 py-12   max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full  xl:mr-0  2xl:col-span-2 ">
+    <div className="bg-gray-300 min-h-screen">
+      <div className="bg-gray-100 grid lg:grid-cols-2 2xl:grid-cols-5">
+        {/* Left Section */}
+        <div className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2">
           <div className="xl:max-w-xl">
+            <img className="h-10" src={logoWorkcation} alt="logoWorkcation" />
             <img
-              className="h-10   "
-              src={logoWorkcation}
-              alt="logoWorkcation"
-            />
-            <img
-              className="mt-6  rounded-lg shadow-xl sm:h-64 sm:w-full sm:object-cover sm:object-center lg:hidden"
+              className="mt-6 rounded-lg shadow-xl sm:h-64 sm:w-full sm:object-cover sm:object-center lg:hidden"
               src={beachWork}
               alt="beachWork"
             />
-
-            <h1 className=" mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
+            <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
               You can work from anywhere.
               <br />
-              <span className="text-indigo-500 ">Take advatage of it.</span>
+              <span className="text-indigo-500">Take advantage of it.</span>
             </h1>
             <p className="mt-2 text-gray-600 sm:mt-4 sm:text-xl">
               Workcation helps you find work-friendly rentals in beautiful
@@ -41,8 +29,11 @@ export default function Matt() {
               on vacation.
             </p>
             <div className="mt-4 sm:mt-6 space-x-1">
-              <a className="btn btn-primary shadow-lg transform transition hover:-translate-y-0.5" href="#">
-                Book you space
+              <a
+                className="btn btn-primary shadow-lg transform transition hover:-translate-y-0.5"
+                href="#"
+              >
+                Book your space
               </a>
               <a className="btn btn-secondary" href="#">
                 Learn More
@@ -50,7 +41,7 @@ export default function Matt() {
             </div>
           </div>
         </div>
-        {/* 右面 */}
+        {/* Right Section */}
         <div className="hidden relative lg:block 2xl:col-span-3">
           <img
             className="absolute inset-0 w-full h-full object-cover object-center"
@@ -58,6 +49,24 @@ export default function Matt() {
             alt="beachWork"
           />
         </div>
+      </div>
+
+      {/* Popular Destinations Section */}
+      <div className="max-w-md sm:max-w-xl lg:max-w-6xl mx-auto px-8 lg:px-12 py-8">
+        <h2 className="text-xl text-gray-900">Popular destinations</h2>
+        <p className="mt-2 text-gray-600">
+          A selection of great work-friendly cities with lots to see and
+          explore.
+        </p>
+        <div className="mt-6 grid gap-6 lg:grid-cols-2 xl:grid-cols-3">
+          {popularDestination.map((destination) => (
+            <DestinationCard key={destination.city} destination={destination} />
+          ))}
+
+          {popularDestination.map((d)=>( <DestinationCard key={d.city} destination={d} />))}
+
+        </div>
+        
       </div>
     </div>
   );
