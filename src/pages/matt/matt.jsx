f9a7ff3e86c6bd05dc99708194d38c68ee082db5
Matt0244 <87352633+Matt0244@users.noreply.github.com>
2024-08-27 11:48:19 +0930

    section 3
    section 3

--- src/pages/matt/matt.jsx
+++ src/pages/matt/matt.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import {
   ArrowPathIcon,
@@ -14,22 +14,49 @@ import beachWork from "../../assets/images/beach-work.jpg";
 
 export default function Matt() {
   return (
-    <div className="bg-gray-100">
-      <div className="px-8 py-12 ">
-        <img className="h-10" src={logoWorkcation} alt="logoWorkcation" />
-        <img
-          className="mt-6  rounded-lg shadow-xl"
-          src={beachWork}
-          alt="beachWork"
-        />
-        <h1 className=" mt-6 text-2xl font-bold text-gray-900">
-          You can work from anywhere. <br />
-          <span className="text-indigo-500">Take advatage of it.</span>
-        </h1>
-        <p className="mt-2 text-gray-600">Workcation helps you find work-friendly rentals in beautiful locations so you can enjoy some
-        nice weather even when you're not on vacation.</p>
-        <div className="mt-4"> 
-          <a className="inline-block px-5 py-3 rounded-lg bg-indigo-500 text-white  uppercase tracking-wider font-semibol text-sm"  href="#">Book you space </a>
+    <div className="bg-gray-300 min-h-screen  ">
+      <div className="bg-gray-100 grid lg:grid-cols-2 2xl:grid-cols-5 ">
+        {/* 左面 */}
+        <div className=" px-8 py-12   max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full  xl:mr-0  2xl:col-span-2 ">
+          <div className="xl:max-w-xl">
+            <img
+              className="h-10   "
+              src={logoWorkcation}
+              alt="logoWorkcation"
+            />
+            <img
+              className="mt-6  rounded-lg shadow-xl sm:h-64 sm:w-full sm:object-cover sm:object-center lg:hidden"
+              src={beachWork}
+              alt="beachWork"
+            />
+
+            <h1 className=" mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
+              You can work from anywhere.
+              <br />
+              <span className="text-indigo-500">Take advatage of it.</span>
+            </h1>
+            <p className="mt-2 text-gray-600 sm:mt-4 sm:text-xl">
+              Workcation helps you find work-friendly rentals in beautiful
+              locations so you can enjoy some nice weather even when you're not
+              on vacation.
+            </p>
+            <div className="mt-4 sm:mt-6">
+              <a
+                className="inline-block px-5 py-3 rounded-lg bg-indigo-500 text-white  uppercase tracking-wider font-semibol text-sm sm:text-base"
+                href="#"
+              >
+                Book you space
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* 右面 */}
+        <div className="hidden relative lg:block 2xl:col-span-3">
+          <img
+            className="absolute inset-0 w-full h-full object-cover object-center"
+            src={beachWork}
+            alt="beachWork"
+          />
         </div>
       </div>
     </div>
