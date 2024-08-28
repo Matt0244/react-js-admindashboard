b417a94bf7257c6d94f18c89650e34ac6ba944a6
Matt0244 <87352633+Matt0244@users.noreply.github.com>
2024-08-28 12:29:41 +0930

    section 8 tailwind config 开启 优化css 在打包后
    section 8 tailwind config 开启 优化css 在打包后

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,17 @@
 // }
 
 module.exports = {
+  purge: {
+    content: ["./src/**/*.jsx", "./index.html"],//  css 优化 保留值需要的文件被定义的目录
+  },
   content: [
     './src/**/*.{js,jsx,ts,tsx}', // 根据你的项目结构调整路径
   ],
+  darkMode: "false", // or 'media' or 'class'   // fales 静止  media根据系统   class 在标签内定义 class="dark"
   theme: {
+
+
+    
     // 在原定breakpoints 上在添加2个
     screen:{
       tablet:'960px',
